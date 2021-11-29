ed708b14293fa410f290ddecae7b49879d8f8688
irlv <191221@ids.upchiapas.edu.mx>
2021-11-29 16:03:08 -0600

    first commit

---
+++ src/app/http.service.ts
@@ -0,0 +1,54 @@
+
+import {Injectable} from '@angular/core';
+import {environment} from "../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  rutaServidor = environment.baseUrl;
+
+  constructor() {
+  }
+
+  public async post(ruta: string, payload: any) {
+    console.log("Hola2")
+    const respuestaRaw = await fetch(this.rutaServidor + ruta, {
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+        // "Access-Control-Allow-Headers":"Content-Type",
+        // "Access-Control-Allow-Origin":"*",
+        // "Access-Control-Allow-Methods":"GET",
+      },
+      method: "POST",
+      credentials: "include",
+    });
+    
+    return await respuestaRaw.json();
+  }
+
+  public async formdata(ruta: string, payload: FormData) {
+    const respuestaRaw = await fetch(this.rutaServidor + ruta, {
+      body: payload,
+      method: "POST",
+    });
+    return await respuestaRaw.json();
+  }
+
+  async get(ruta: string) {
+    // Por defecto se hace una petición GET
+    const respuestaRaw = await fetch(this.rutaServidor + ruta, {
+      credentials: "include",
+    });
+    return await respuestaRaw.json();
+  }
+
+  async delete(ruta: string) {
+    const respuestaRaw = await fetch(this.rutaServidor + ruta, {
+      credentials: "include",
+      method: "DELETE",
+    });
+    return await respuestaRaw.json();
+  }
+}
---
+++ src/app/productos.service.ts
@@ -0,0 +1,44 @@
+
+import {Injectable} from '@angular/core';
+import {Producto} from "./producto";
+import {HttpService} from "./http.service";
+import { HttpClientModule, HttpHeaders } from '@angular/common/http'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  constructor(private http: HttpService) {
+  }
+
+  public async eliminarProducto(idProducto) {
+    return await this.http.delete("/producto?id=".concat(idProducto));
+  }
+  
+
+  public async agregarProducto(producto: Producto) {
+    const headers = new HttpHeaders();
+   console.log("Hola")
+    return await this.http.post("/producto", producto);
+  }
+
+  /*
+  El formdata debe tener el id del producto
+   */
+  public async agregarFotosDeProducto(fotos: FormData) {
+    return await this.http.formdata("/fotos_producto", fotos);
+  }
+
+  public async obtenerProductos() {
+    return await this.http.get("/productos");
+  }
+
+  public async obtenerProductosConFotos() {
+    return await this.http.get("/productos_con_fotos");
+  }
+
+  public async obtenerProductoConFotosPorId(idProducto) {
+    return await this.http.get("/producto?id=".concat(idProducto));
+  }
+}
